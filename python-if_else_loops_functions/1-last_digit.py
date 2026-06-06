15a41dac26826bf4c712a949668eb830ba507383
shathaaa1110-arch <shathaaa1110@gmail.com>
2026-06-06 23:13:09 +0300

    Extract the last digit of a number and evaluate it

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+if number < 0:
+    last_digit = -(abs(number) % 10)
+else:
+    last_digit = number % 10
+
+message = f"Last digit of {number} is {last_digit} and is"
+
+if last_digit > 5:
+    print(f"{message} greater than 5")
+elif last_digit == 0:
+    print(f"{message} 0")
+else:
+    print(f"{message} less than 6 and not 0")
