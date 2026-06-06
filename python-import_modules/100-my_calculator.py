16f3af92da9bce28859fd404476237509d3a658f
shathaaa1110-arch <shathaaa1110@gmail.com>
2026-06-06 23:57:56 +0300

    Fix syntax and import errors for my_calculator

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-import sys
+from sys import argv, exit
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
 
     ops = {"+": add, "-": sub, "*": mul, "/": div}
-    
-    if sys.argv[2] not in ops:
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    op = sys.argv[2]
-    
+    if argv[2] not in ops:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    op = argv[2]
+
     result = ops[op](a, b)
     print("{} {} {} = {}".format(a, op, b, result))
