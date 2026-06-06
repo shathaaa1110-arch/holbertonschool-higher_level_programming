fd2c498c8a2b2a9498d9378ff82b73cad01fca80
shathaaa1110-arch <shathaaa1110@gmail.com>
2026-06-06 23:54:08 +0300

    Build a dynamic calculator using imported functions and sys.argv

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    
+    if sys.argv[2] not in ops:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    op = sys.argv[2]
+    
+    result = ops[op](a, b)
+    print("{} {} {} = {}".format(a, op, b, result))
