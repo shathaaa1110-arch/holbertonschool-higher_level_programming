0b987b606c03a2227158d49aa929a9228d1c9286
shathaaa1110-arch <shathaaa1110@gmail.com>
2026-06-13 22:11:50 +0300

    fix

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,29 +1,5 @@
 #!/usr/bin/python3
-"""
-Module: 8-simple_delete
-Deletes a key in a dictionary.
-"""
-
-
 def simple_delete(a_dictionary, key=""):
-    """
-    Deletes a key in a dictionary.
-
-    Args:
-        a_dictionary: A dictionary
-        key: The key to delete (always a string, default is empty string)
-
-    Returns:
-        The updated dictionary.
-        If the key doesn't exist, the dictionary won't change.
-
-    Example:
-        >>> a_dictionary = {'language': "C", 'track': "Low"}
-        >>> new_dict = simple_delete(a_dictionary, 'track')
-        >>> print(new_dict)
-        {'language': 'C'}
-    """
     if key in a_dictionary:
         del a_dictionary[key]
     return a_dictionary
-
