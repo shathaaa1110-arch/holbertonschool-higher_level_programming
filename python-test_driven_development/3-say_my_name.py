6b50eeac373661f983ba2659b919406a29fa35ed
shathaaa1110-arch <shathaaa1110@gmail.com>
2026-06-13 23:41:05 +0300

    Fix formatting and whitespace in say_my_name function

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -7,11 +7,11 @@ This module contains a function that prints a name
 def say_my_name(first_name, last_name=""):
     """
     Prints 'My name is <first_name> <last_name>'
-    
+
     Args:
         first_name: First name (must be a string)
         last_name: Last name (must be a string, defaults to empty)
-        
+
     Raises:
         TypeError: If first_name or last_name are not strings
     """
@@ -19,6 +19,6 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    
+
     print("My name is {} {}".format(first_name, last_name))
     
