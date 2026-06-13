2b9b3019210bd070fe5147fc572de4cd482b2c55
shathaaa1110-arch <shathaaa1110@gmail.com>
2026-06-13 23:37:13 +0300

    Refactor say_my_name function documentation
    Updated docstring to clarify function usage and argument types.

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-"""Module for printing a name."""
+"""
+This module contains a function that prints a name
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """Print 'My name is <first_name> <last_name>'.
-
-    Raises TypeError if either argument is not a string.
     """
-    if not isinstance(first_name, str):
+    Prints 'My name is <first_name> <last_name>'
+    
+    Args:
+        first_name: First name (must be a string)
+        last_name: Last name (must be a string, defaults to empty)
+        
+    Raises:
+        TypeError: If first_name or last_name are not strings
+    """
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
+    
     print("My name is {} {}".format(first_name, last_name))
