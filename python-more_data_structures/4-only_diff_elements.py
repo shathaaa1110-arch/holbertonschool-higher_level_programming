afb3a49752b34e9ea4137571b4265968cdd23707
shathaaa1110-arch <shathaaa1110@gmail.com>
2026-06-13 22:04:55 +0300

    Save all local changes and fixes

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,27 +1,3 @@
 #!/usr/bin/python3
-"""
-Module: 4-only_diff_elements
-Returns a set of all elements present in only one set.
-"""
-
-
 def only_diff_elements(set_1, set_2):
-    """
-    Returns a set of all elements present in only one set.
-
-Args:
-        set_1: First set
-        set_2: Second set
-
-Returns:
-        A new set containing elements that are in set_1 or set_2 but not in both.
-        This is the symmetric difference of the two sets.
-
-Example:
-        >>> set_1 = {"Python", "C", "Javascript"}
-        >>> set_2 = {"Bash", "C", "Ruby", "Perl"}
-        >>> od_set = only_diff_elements(set_1, set_2)
-        >>> print(sorted(list(od_set)))
-        ['Bash', 'Javascript', 'Perl', 'Python', 'Ruby']
-    """
-    return (set_1 - set_2) | (set_2 - set_1)
+    return set_1 ^ set_2
