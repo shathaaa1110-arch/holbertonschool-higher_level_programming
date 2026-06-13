22bcbc7b1aa2d829d1db86d2315ba05f5c93a403
shathaaa1110-arch <shathaaa1110@gmail.com>
2026-06-13 23:54:35 +0300

    Update 5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 """
-This module provides a function to format and indent text.
+This module contains a function that indents text.
 """
 
 
 def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of these characters: ., ? and :
+
+    Args:
+        text (str): The text to be printed.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
     if type(text) is not str:
         raise TypeError("text must be a string")
 
