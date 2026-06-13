c6738dc7bde2d84724046f8cf87aed84569f86b8
shathaaa1110-arch <shathaaa1110@gmail.com>
2026-06-13 21:57:16 +0300

    Modify docstring example in simple_delete function
    Updated example in docstring to reflect changes in dictionary.

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -18,15 +18,12 @@ def simple_delete(a_dictionary, key=""):
         If the key doesn't exist, the dictionary won't change.
 
     Example:
-        >>> a_dictionary = {'language': "C", 'Number': 89, 'track': "Low", 'ids': [1, 2, 3]}
+        >>> a_dictionary = {'language': "C", 'track': "Low"}
         >>> new_dict = simple_delete(a_dictionary, 'track')
         >>> print(new_dict)
-        {'language': 'C', 'Number': 89, 'ids': [1, 2, 3]}
-
-        >>> new_dict = simple_delete(a_dictionary, 'c_is_fun')
-        >>> print(new_dict)
-        {'language': 'C', 'Number': 89, 'ids': [1, 2, 3]}
+        {'language': 'C'}
     """
     if key in a_dictionary:
         del a_dictionary[key]
     return a_dictionary
+
