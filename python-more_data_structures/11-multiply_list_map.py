f0c6bf5930b2a5c3db5fa55ff7db2328a61f25fb
shathaaa1110-arch <shathaaa1110@gmail.com>
2026-06-13 21:59:33 +0300

    Refine docstrings in multiply_list_map function
    Updated docstrings for clarity and consistency.

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """
 Module: 11-multiply_list_map
-Returns a list with all values multiplied by a number without using any loops.
+Returns a list with all values multiplied by a number without using loops.
 """
 
 
 def multiply_list_map(my_list=[], number=0):
     """
-    Returns a list with all values multiplied by a number without using any loops.
+    Returns a list with all values multiplied by a number without loops.
 
     Args:
         my_list: The initial list
         number: The number to multiply by
 
     Returns:
-        A new list with the same length where each value is multiplied by number.
+        A new list with the same length where each value is multiplied.
         The initial list is not modified.
 
     Note:
@@ -26,7 +26,6 @@ def multiply_list_map(my_list=[], number=0):
         >>> new_list = multiply_list_map(my_list, 4)
         >>> print(new_list)
         [4, 8, 12, 16, 24]
-        >>> print(my_list)
-        [1, 2, 3, 4, 6]
     """
     return list(map(lambda x: x * number, my_list))
+
