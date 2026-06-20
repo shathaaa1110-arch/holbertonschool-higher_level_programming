d70e4f204a72daf525fee3090439e96d7c2b614d
Shatha Alanazi <shathaaa1110@gmail.com>
2026-06-20 03:54:12 +0300

    Refactor __repr__ method for Rectangle class

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -63,7 +63,7 @@ class Rectangle:
 
     def __repr__(self):
         """Return the string representation of the Rectangle."""
-        rect = "Rectangle(" + str(self.__width) + ", " + str(self.__height) + ")"
+        rect = "Rectangle({}, {})".format(self.__width, self.__height)
         return (rect)
 
     def __del__(self):
