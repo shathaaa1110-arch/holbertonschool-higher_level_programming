a2f1a04b79b13e2e822e28a8c15d80bc2dfd32ac
Shatha Alanazi <shathaaa1110@gmail.com>
2026-06-20 03:51:18 +0300

    Refactor __repr__ method for better formatting

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -60,5 +60,5 @@ class Rectangle:
 
     def __repr__(self):
         """Return the string representation of the Rectangle."""
-        rect = "Rectangle(" + str(self.__width) + ", " + str(self.__height) + ")"
+        rect = "Rectangle({}, {})".format(self.__width, self.__height)
         return (rect)
