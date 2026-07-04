dfbfd7405013617e99ecbee652d78fa314133505
Shatha Alanazi <shathaaa1110@gmail.com>
2026-07-04 13:29:54 +0300

    Replace append_write with to_json_string function

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""Module that appends a string to a text file."""
+"""Module that converts an object to a JSON string."""
+
+import json
 
 
-def append_write(filename="", text=""):
-    """Append a string to a UTF-8 text file.
-
-    Returns:
-        The number of characters added.
-    """
-    with open(filename, "a", encoding="utf-8") as file:
-        return file.write(text)
+def to_json_string(my_obj):
+    """Return the JSON representation of an object."""
+    return json.dumps(my_obj)
