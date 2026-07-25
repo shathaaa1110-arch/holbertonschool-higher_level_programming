19211b942af26b0db0e9df3188fcfb8b7fdfffd4
Shatha Alanazi <shathaaa1110@gmail.com>
2026-07-25 21:47:22 +0300

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,38 @@
 #!/usr/bin/python3
-"""Lists all states from a MySQL database."""
-
+"""
+Script that lists all states from the database hbtn_0e_0_usa
+"""
 import sys
 import MySQLdb
 
-
 if __name__ == "__main__":
-    connection = MySQLdb.connect(
+    # الحصول على معلومات الاتصال من مدخلات سطر الأوامر (Arguments)
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # الاتصال بقاعدة البيانات MySQL
+    db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=mysql_user,
+        passwd=mysql_password,
+        db=db_name
     )
 
-    cursor = connection.cursor()
+    # إنشاء مؤشر (Cursor) لتنفيذ أوامر SQL
+    cursor = db.cursor()
+
+    # تنفيذ الاستعلام لجلب كل الولايات وترتيبها تصاعدياً حسب الـ id
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    for state in cursor.fetchall():
-        print(state)
+    # جلب جميع النتائج
+    rows = cursor.fetchall()
 
+    # طباعة النتائج بالشكل المطلوب
+    for row in rows:
+        print(row)
+
+    # إغلاق المؤشر والاتصال بقاعدة البيانات
     cursor.close()
-    connection.close()
+    db.close()
