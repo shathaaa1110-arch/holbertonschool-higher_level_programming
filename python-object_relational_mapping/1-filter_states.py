5fcaf2ad22293df5fd73102c252180e1a883f466
Shatha Alanazi <shathaaa1110@gmail.com>
2026-07-25 21:30:25 +0300

    Add script to filter states starting with 'N'
    This script connects to a MySQL database and lists states whose names start with an uppercase 'N'.

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Lists states whose names start with an uppercase N."""
+
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = connection.cursor()
+    cursor.execute(
+        "SELECT * FROM states "
+        "WHERE name LIKE BINARY 'N%' "
+        "ORDER BY id ASC"
+    )
+
+    for state in cursor.fetchall():
+        print(state)
+
+    cursor.close()
+    connection.close()
